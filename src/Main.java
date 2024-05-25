f1e45e0ff7fddc10ab292a04f1c5bc0b4de17742
SoloMaksimilian <mephistosmad@gmail.com>
2024-05-26 02:35:34 +0300

    Ex2

--- src/Main.java
+++ src/Main.java
@@ -15,11 +15,21 @@ public class Main {
             a++;
             System.out.print(a + " ");
         }
-        System.out.println("");
+        System.out.println(" ");
         while (a >= 1) {
             System.out.print(a + " ");
             a--;
         }
+        System.out.println(" ");
 
+        int population = 12000000;
+        int fertility=0;
+        int mortality=0;
+        for (int year = 0; year<=10; year++) {
+            System.out.println(population);
+            population = population+fertility-mortality;
+            fertility = population / 1000 * 17;
+            mortality = population / 1000 * 8;
+            }
     }
 }
